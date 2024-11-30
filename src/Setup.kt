9ac9773e787e5cd7a634ef61c4a870e9f6f9ff61
Ulrich Kemp <ulrich@kemp.in>
2024-11-30 13:14:30 +0100

    Added Setup.kt

---
+++ src/Setup.kt
@@ -0,0 +1,39 @@
+import java.io.FileNotFoundException
+import kotlin.io.path.Path
+import kotlin.io.path.absolute
+import kotlin.io.path.createDirectory
+import kotlin.io.path.createFile
+import kotlin.io.path.exists
+import kotlin.io.path.useLines
+import kotlin.io.path.writeLines
+
+fun main() {
+    val src = Path("./src").absolute()
+    val template = src.resolve("template/DayXX.kt")
+    if (!template.exists()) {
+        throw FileNotFoundException("Missing $template")
+    }
+
+    for (day in 1..25) {
+        val day = String.format("%02d", day)
+        val packageName = "day$day"
+        val path = src.resolve(packageName)
+
+        if (!path.exists()) {
+            path.createDirectory()
+            val dst = path.resolve("Day$day.kt")
+            template.useLines { lines ->
+                dst.writeLines(lines.map { line -> if (line.startsWith("package")) "package $packageName" else line })
+            }
+        } else {
+            println("Skipping $path...")
+        }
+
+        listOf(
+            src.resolve("Day${day}_test.txt"),
+            src.resolve("Day$day.txt")
+        ).forEach { txt ->
+            if (!txt.exists()) txt.createFile()
+        }
+    }
+}
--- src/Utils.kt
+++ src/Utils.kt
@@ -15,7 +15,10 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
     .toString(16)
     .padStart(32, '0')
 
-/**
- * The cleaner shorthand for printing output.
- */
-fun Any?.println() = println(this)
+fun log(value: Any?) {
+    if (value != null) {
+        println(value)
+    } else {
+        println("null")
+    }
+}
