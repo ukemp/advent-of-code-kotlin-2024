2570f44f83f2b5d31e35615ffd8f4f612e1fa476
Ulrich Kemp <ulrich@kemp.in>
2024-12-01 13:46:12 +0100

    Solved Day01

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,21 +1,50 @@
 package day01
 
 import readInput
+import kotlin.math.absoluteValue
+import kotlin.to
 
 fun main() {
+
+    fun readInput(input: List<String>): Pair<MutableList<Int>, MutableList<Int>> {
+        return input.fold(mutableListOf<Int>() to mutableListOf<Int>()) { ids, line ->
+            ids.first.add(line.substringBefore(' ').toInt())
+            ids.second.add(line.substringAfter(' ').trim().toInt())
+            ids
+        }
+    }
+
     fun part1(input: List<String>): Long {
-        return input.size.toLong()
+        val (left, right) = readInput(input)
+        left.sort()
+        right.sort()
+
+        return left.mapIndexed { index, value -> (value - right[index]).absoluteValue }.sum().toLong()
     }
 
     fun part2(input: List<String>): Long {
-        return input.size.toLong()
+        val (left, right) = readInput(input)
+        left.sort()
+        right.sort()
+
+        var startIndex = 0
+        return left.sumOf { id ->
+            var count = 0
+            for (index in startIndex..<right.size) {
+                if (right[index] == id) {
+                    count++
+                } else if (right[index] > id) {
+                    startIndex = index
+                    break
+                }
+            }
+            count * id
+        }.toLong()
     }
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput).also(::println) == 1L)
+    check(part1(testInput).also(::println) == 11L)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
