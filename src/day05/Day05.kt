ee5bdaecda7cc6fb85d442de0cc925117dde1731
Ulrich Kemp <ulrich@kemp.in>
2024-12-03 13:41:10 +0100

    Day03

---
+++ src/day05/Day05.kt
@@ -0,0 +1,28 @@
+package day05
+
+import readInput
+import kotlin.time.measureTime
+
+fun main() {
+    fun part1(input: List<String>): Long {
+        return input.size.toLong()
+    }
+
+    fun part2(input: List<String>): Long {
+        return input.size.toLong()
+    }
+
+    val testInput = readInput("Day05_test")
+    check(part1(testInput).also(::println) == 1L)
+    //check(part2(testInput).also(::println) == 1L)
+
+    val input = readInput("Day05")
+
+    measureTime {
+        println(part1(input))
+    }.also { println("Solved part 1 in $it") }
+
+    measureTime {
+        println(part2(input))
+    }.also { println("Solved part 2 in $it") }
+}
