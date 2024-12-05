00c38da10638ee86ac40f8c0da6a249f28c67de0
Ulrich Kemp <ulrich@kemp.in>
2024-12-05 07:00:29 +0100

    Day05

--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -4,17 +4,76 @@ import readInput
 import kotlin.time.measureTime
 
 fun main() {
+
+    fun parseRules(input: List<String>): List<Pair<Int, Int>> {
+        return input.mapNotNull { line ->
+            if (line.contains("|")) {
+                line.substringBefore('|').toInt() to line.substringAfter('|').toInt()
+            } else {
+                null
+            }
+        }
+    }
+
+    fun indexesOf(pages: List<Int>, rule: Pair<Int, Int>) =
+        pages.indexOf(rule.first) to pages.indexOf(rule.second)
+
+    fun isOrdered(pages: List<Int>, rules: List<Pair<Int, Int>>): Boolean {
+        return rules.all { rule ->
+            val (i1, i2) = indexesOf(pages, rule)
+            !(i1 != -1 && i2 != -1 && i1 >= i2)
+        }
+    }
+
+    fun MutableList<Int>.swap(i1: Int, i2: Int) {
+        val v1 = get(i1)
+        set(i1, get(i2))
+        set(i2, v1)
+    }
+
     fun part1(input: List<String>): Long {
-        return input.size.toLong()
+        val rules = parseRules(input)
+        return input.sumOf { line ->
+            if (line.contains(",")) {
+                val pages = line.split(",").map { it.toInt() }
+                if (isOrdered(pages, rules)) pages[pages.size / 2].toLong() else 0L
+            } else {
+                0L
+            }
+        }
     }
 
     fun part2(input: List<String>): Long {
-        return input.size.toLong()
+        val rules = parseRules(input)
+        return input.sumOf { line ->
+            if (line.contains(",")) {
+                val pages = line.split(",").map { it.toInt() }.toMutableList()
+                var isOrdered = isOrdered(pages, rules)
+                if (isOrdered) {
+                    0L
+                } else {
+                    while (!isOrdered(pages, rules)) {
+                        rules.forEach { rule ->
+                            val (i1, i2) = indexesOf(pages, rule)
+                            if (i1 != -1 && i2 != -1) {
+                                if (i1 >= i2) {
+                                    pages.swap(i1, i2)
+                                }
+                            }
+                        }
+                    }
+
+                    pages[pages.size / 2].toLong()
+                }
+            } else {
+                0L
+            }
+        }
     }
 
     val testInput = readInput("Day05_test")
-    check(part1(testInput).also(::println) == 1L)
-    //check(part2(testInput).also(::println) == 1L)
+    check(part1(testInput).also(::println) == 143L)
+    check(part2(testInput).also(::println) == 123L)
 
     val input = readInput("Day05")
 
